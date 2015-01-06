bb9d5083815ea2631df5e69f34e6b2f94465ba19
Python15 <py@iMacdeAePereira.home>
2015-01-06 17:02:04 +0100

    MaJ - Client mobile

---
+++ Client Mobile/Seen/Seen/Compte.swift
@@ -0,0 +1,26 @@
+//
+//  Compte.swift
+//  Seen
+//
+//  Created by Cyril Py on 06/01/2015.
+//  Copyright (c) 2015 Cyril Py. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class Compte: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
